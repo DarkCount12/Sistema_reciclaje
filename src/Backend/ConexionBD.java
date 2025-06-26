cae4648ea81a22bf4eafec1bc72e9bab93714ea2
Alejandra786 <alejandravargasmendez0@gmail.com>
2025-06-26 16:15:24 -0400

    fix: remove password connection db

--- src/Backend/ConexionBD.java
+++ src/Backend/ConexionBD.java
@@ -5,10 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
-    // Cambia "sistema_reciclaje" por el nombre real de tu base de datos local si es diferente
     private static final String URL = "jdbc:mysql://localhost:3306/sistema_reciclaje?useSSL=false&serverTimezone=UTC";
     private static final String USUARIO = "root";
-    private static final String CONTRASENA = "IJgonaldos"; // Cambia si tu MySQL tiene contraseña
+    private static final String CONTRASENA = ""; 
 
     public static Connection obtenerConexion() {
         try {
